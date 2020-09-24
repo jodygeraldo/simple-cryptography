950005d2f78a1afae3d4f4a1919b6adadb21d34e
Jody Geraldo <ajody.geraldo@gmail.com>
2020-09-24 23:49:01 +0700

    Init

---
+++ public/playingAround.js
@@ -0,0 +1,68 @@
+const plainText = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '
+]
+
+let cipherText = []
+
+let encodedString = ''
+let decodedString = ''
+
+const encode = (text, k = 3) => {
+  const textArray = text.split('')
+  const encoded = []
+  const plainIndex = []
+
+  getCipherText(k)
+
+  for (const el of textArray) {
+    plainIndex.push(plainText.indexOf(el))
+  }
+  for (const el of plainIndex) {
+    encoded.push(cipherText[el])
+  }
+  encodedString = encoded.join('')
+  document.getElementById('encodedText').value = encodedString
+}
+
+const decode = (text, k = 3) => {
+  const textArray = text.split('')
+  const decoded = []
+  const cipherIndex = []
+
+  getCipherText(k)
+
+  for (const el of textArray) {
+    cipherIndex.push(cipherText.indexOf(el))
+  }
+  for (const el of cipherIndex) {
+    decoded.push(plainText[el])
+  }
+  decodedString = decoded.join('')
+  document.getElementById('decodedText').value = decodedString
+}
+
+const getCipherText = (k) => {
+  const basePositions = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '
+  ]
+
+  const spliced = basePositions.splice(0, k)
+  cipherText = basePositions.concat(spliced)
+}
+
+document.getElementById('encode').addEventListener('click', (e) => {
+  e.preventDefault()
+  const encodedInput = document.getElementById('encodedInput').value
+  const encodedKeyInput = document.getElementById('encodedKeyInput').value
+  document.getElementById('originalText').value = encodedInput
+  encode(encodedInput, +encodedKeyInput)
+  document.getElementById('encodedInput').value = ''
+})
+
+document.getElementById('decode').addEventListener('click', (e) => {
+  e.preventDefault()
+  const decodedInput = document.getElementById('decodedInput').value
+  const decodedKeyInput = document.getElementById('decodedKeyInput').value
+  decode(decodedInput, +decodedKeyInput)
+  document.getElementById('decodedInput').value = ''
+})
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Description
+
+Simple cryptography web apps made for assignment project.
+
+## Usage
+
+[Simply click this link](https://jody-cryptography.web.app/)
